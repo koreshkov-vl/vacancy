10d170a1060a106a950144c1a7f2849499c58eba
Vladimir Koreshkov <koreshkov.vl@gmail.com>
2019-04-27 15:11:54 +0300

    create vacancy table migration

---
+++ database/migrations/2019_04_27_115309_create_vacancy_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacancyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancy', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('is_deleted')->default(0);
+            $table->unsignedInteger('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacancy');
+    }
+}

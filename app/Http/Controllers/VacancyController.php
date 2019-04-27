8e7ce1d546f0af570af34128f22410add991b4e5
Vladimir Koreshkov <koreshkov.vl@gmail.com>
2019-04-28 02:55:20 +0300

    admin panel functionality

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Vacancy;
+use Illuminate\Support\Facades\Session;
 
 class VacancyController extends Controller
 {
+    const PAGINATION_CONST = 10;
+
     public function index() {
 
         $vacancies = Vacancy::getNewest();
@@ -14,14 +18,23 @@ class VacancyController extends Controller
         return view('vacancies.index', compact('vacancies'));
     }
 
-    public function show(Vacancy $vacancy)
+    public function show(int $vacancy)
     {
+        $vacancy = Vacancy::getWithOwner($vacancy);
+
         return view('vacancies.show', compact('vacancy'));
     }
 
     public function create()
     {
         return view('vacancies.create');
+    }
+
+    public function admin()
+    {
+        $vacancies = Vacancy::pagination(self::PAGINATION_CONST);
+
+        return view('vacancies.admin', compact('vacancies'));
     }
 
     public function store() {
@@ -32,6 +45,23 @@ class VacancyController extends Controller
 
         auth()->user()->vacancies()->create($attributes);
 
+        Session::flash('message.level', 'success');
+        Session::flash('message.content', 'Created');
+
         return redirect('/vacancies');
+    }
+
+    public function delete(int $vacancy)
+    {
+        try {
+            Vacancy::where('id', $vacancy)->delete();
+            Session::flash('message.level', 'info');
+            Session::flash('message.content', 'Deleted');
+        } catch (\Exception $e) {
+            Session::flash('message.level', 'danger');
+            Session::flash('message.content', 'Failure!');
+        }
+
+        return redirect('/vacancies/admin');
     }
 }
--- app/Vacancy.php
+++ app/Vacancy.php
@@ -14,15 +14,45 @@ class Vacancy extends Model
         return "/vacancies/{$this->id}";
     }
 
-    public static function getNewest($count = 6)
+    public static function getNewest(int $count = 6)
     {
         $vacancies = DB::table('vacancies')
             ->orderBy('created_at', 'desc')
             ->limit($count)
-            ->selectRaw('*, CONCAT(\'/vacancies/\', id) as path')
+            ->selectRaw('vacancies.*, CONCAT(\'/vacancies/\', vacancies.id) as path')
             ->get();
 
         return $vacancies;
     }
 
+    public static function pagination(int $paginationCount)
+    {
+        $vacancies = DB::table('vacancies')
+            ->join('users', 'users.id', '=', 'vacancies.owner_id')
+            ->orderBy('created_at', 'desc')
+            ->selectRaw(
+                'vacancies.*, 
+                users.name as user_name, 
+                users.email as user_email, 
+                CONCAT(\'/vacancies/\', vacancies.id) as path,
+                CONCAT(\'/vacancies/\', vacancies.id, \'/delete\') as delete_path')
+            ->paginate($paginationCount);
+
+        return $vacancies;
+    }
+
+    public static function getWithOwner(int $vacancyId)
+    {
+        $vacancy = DB::table('vacancies')
+            ->join('users', 'users.id', '=', 'vacancies.owner_id')
+            ->where('vacancies.id', $vacancyId)
+            ->selectRaw('
+                vacancies.*,
+                users.name as user_name,
+                users.email as user_email,
+                CONCAT(\'/vacancies/\', vacancies.id, \'/delete\') as delete_path')
+            ->first();
+
+        return $vacancy;
+    }
 }
